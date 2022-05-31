e6c289f3307aecc2ec176e7b186051a21a162e91
Insnesia <80085808+insnesia@users.noreply.github.com>
2022-05-31 20:25:15 +0700

    Update desc.js

--- desc.js
+++ desc.js
@@ -1,18 +1,18 @@
   function inpo() {
-  fetch(`https://api.telegra.ph/getPage/inspo-05-31?return_content=false`,{method: &#39;GET&#39;})
-  .then((response) =&gt; {
+  fetch(`https://api.telegra.ph/getPage/inspo-05-31?return_content=false`,{method: 'GET'})
+  .then((response) => {
     return response.json()
   })
-  .then((data) =&gt; {
-	document.getElementById(&quot;info&quot;).innerHTML = data.result.description;
+  .then((data) => {
+	document.getElementById("info").innerHTML = data.result.description;
    iples();
   })
-  .catch((err) =&gt; {
+  .catch((err) => {
     // Do something for an error here
   })
  
 }
   
-  window.addEventListener(&#39;DOMContentLoaded&#39;, (event) =&gt; {
+  window.addEventListener('DOMContentLoaded', (event) => {
      inpo();
 });
